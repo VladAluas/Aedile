731816f276f201a02b8667c412d12c21a99d22fa
VladAluas <vlad.aluas@gmail.com>
2026-08-04 10:27:52 +0300

    added DatabaseConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,10 @@
 package config
 
 type Config struct {
-	Project string        `yaml: "project"`
-	Compose ComposeConfig `yaml: "compose"`
-	Docker  DockerConfig  `yaml: "docker"`
+	Project  string         `yaml:"project"`
+	Compose  ComposeConfig  `yaml:"compose"`
+	Docker   DockerConfig   `yaml:"docker"`
+	Database DatabaseConfig `yaml:"database"`
 }
 
 type ComposeConfig struct {
@@ -12,4 +13,12 @@ type ComposeConfig struct {
 
 type DockerConfig struct {
 	Project string `yaml:"project"`
+}
+
+type DatabaseConfig struct {
+	User string `yaml:"user"`
+	Host string `yaml:"host"`
+	Pass string `yaml:"pass"`
+	Port int    `yaml:"port"`
+	Name string `yaml:"name"`
 }
