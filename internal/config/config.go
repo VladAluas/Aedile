8bb1e53b22be312e852badbb7c6e1ec2573af4fb
VladAluas <vlad.aluas@gmail.com>
2026-08-07 11:53:09 +0300

    structs for the config file
    This will be updated in the future with additional services. It's still
    unstable since I've not decided on the final services. I might need to
    break them into smaller files

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,18 @@
 package config
 
 type Config struct {
-	Project  string         `yaml:"project"`
-	Compose  ComposeConfig  `yaml:"compose"`
-	Docker   DockerConfig   `yaml:"docker"`
-	Database DatabaseConfig `yaml:"database"`
+	Project   ProjectConfig   `yaml:"project"`
+	Compose   ComposeConfig   `yaml:"compose"`
+	Docker    DockerConfig    `yaml:"docker"`
+	Metadata  MetadataConfig  `yaml:"metadata"`
+	Database  DatabaseConfig  `yaml:"database"`
+	Storage   StorageConfig   `yaml:"storage"`
+	Liquibase LiquibaseConfig `yaml:"liquibase"`
+}
+
+type ProjectConfig struct {
+	Name        string `yaml:"name"`
+	Environment string `yaml:"environment"`
 }
 
 type ComposeConfig struct {
@@ -15,10 +23,43 @@ type DockerConfig struct {
 	Project string `yaml:"project"`
 }
 
+type MetadataConfig struct {
+	Engine string `yaml:"engine"`
+	Image  string `yaml:"image"`
+	User   string `yaml:"user"`
+	Host   string `yaml:"host"`
+	Pass   string `yaml:"pass"`
+	Port   int    `yaml:"port"`
+	Name   string `yaml:"name"`
+}
+
 type DatabaseConfig struct {
-	User string `yaml:"user"`
-	Host string `yaml:"host"`
-	Pass string `yaml:"pass"`
-	Port int    `yaml:"port"`
-	Name string `yaml:"name"`
+	Engine string `yaml:"engine"`
+	Image  string `yaml:"image"`
+	User   string `yaml:"user"`
+	Host   string `yaml:"host"`
+	Pass   string `yaml:"pass"`
+	Port   int    `yaml:"port"`
+	Name   string `yaml:"name"`
+}
+
+type StorageConfig struct {
+	Image       string `yaml:"image"`
+	User        string `yaml:"user"`
+	Pass        string `yaml:"pass"`
+	APIPort     int    `yaml:"api_port"`
+	WebPort     int    `yaml:"web_port"`
+	Container   string `yaml:"container"`
+	Command     string `yaml:"command"`
+	StorageData string `yaml:"storage_data"`
+}
+
+type LiquibaseConfig struct {
+	Image      string `yaml:"image"`
+	Name       string `yaml:"name"`
+	WorkingDir string `yaml:"working_dir"`
+	Changelog  string `yaml:"changelog"`
+	Properties string `yaml:"properties"`
+	Dependency string `yaml:"dependency"`
+	Lib        string `yaml:"lib"`
 }
